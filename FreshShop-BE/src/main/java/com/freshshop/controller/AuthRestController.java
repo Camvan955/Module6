8bafde1335397557c60afb83250382e512f0a9c3
Camvan955 <nguyenvann955@gmail.com>
2023-04-06 21:29:42 +0700

    update code

--- FreshShop-BE/src/main/java/com/freshshop/controller/AuthRestController.java
+++ FreshShop-BE/src/main/java/com/freshshop/controller/AuthRestController.java
@@ -19,9 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
